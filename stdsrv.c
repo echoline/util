0a99fd5c9896f803e0d833c8c3897e426fecbb09
Eli Cohen <echoline@gmail.com>
2014-01-14 11:54:09 -0800

    poll > fork

--- stdsrv.c
+++ stdsrv.c
@@ -1,10 +1,12 @@
 #include "comms.h"
 #include <strings.h>
 #include <unistd.h>
+#include <poll.h>
 
 int main(int argc, char **argv) {
 	char buf[1024];
 	int r, client, sock;
+	struct pollfd fds[2];
 
 	if (argc < 3) {
 		printf("usage: %s [tcp|unix] [port|path]\n", argv[0]);
@@ -14,25 +16,52 @@ int main(int argc, char **argv) {
 	sock = strcasecmp(argv[1], "tcp")? init_unix_listen(argv[2])
 					 : init_tcp_listen(atoi(argv[2]));
 
+	if ((client = accept(sock, NULL, NULL)) > -1) {
+		fds[0].fd = client;
+		fds[1].fd = 0;
+		fds[0].events = POLLIN | POLLHUP;
+		fds[1].events = POLLIN | POLLHUP;
 
-	while ((client = accept(sock, NULL, NULL)) > -1) {
-		switch(fork()) {
-		case 0:
-			while ((r = read(client, buf, sizeof(buf))) > -1) {
+		while (poll(fds, 2, -1)) {
+			if (fds[0].revents & POLLIN) {
+				r = read(client, buf, sizeof(buf));
+				// ctrl+d pressed
+				if (buf[0] == '\x04') {
+					close (client);
+					return 0;
+				}
+
+				if (r < 0) {
+					perror("read");
+					continue;
+				}
 				write(1, buf, r);
 				fflush(stdout);
 			}
-			perror("read");
-			return 0;
-		case -1:
-			perror("fork");
-			break;
-		default:
-			while (fgets(buf, sizeof(buf), stdin)) {
+
+			if (fds[1].revents & POLLIN) {
+				if (fgets(buf, sizeof(buf), stdin) == NULL) {
+					close (client);
+					return 0;
+				}
+
 				r = write(client, buf, strlen(buf));
-				if (r < 0) perror("write");
+				if (r < 0)
+					perror("write");
 			}
-			close(client);
+
+			for (r = 0; r < 2; r++)
+				if (fds[r].revents & POLLHUP) {
+					close (client);
+					return 0;
+				}
 		}
+
+		perror("poll");
+		close(client);
+		return -1;
 	}
+
+	perror ("accept");
+	return -1;
 }
