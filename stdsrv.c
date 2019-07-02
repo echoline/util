54fd3161e5408304a768f9506abfc4e884d38237
Linux User <eli@singularity.my.domain>
2019-07-01 22:12:06 -0700

    stdsrv multi

--- stdsrv.c
+++ stdsrv.c
@@ -3,65 +3,99 @@
 #include <unistd.h>
 #include <poll.h>
 
+int usage(char **argv) {
+	printf("usage: %s tcp|unix portnum|/path/to/socket\n", argv[0]);
+	exit(-1);
+}
+
 int main(int argc, char **argv) {
 	char buf[1024];
 	int r, client, sock;
-	struct pollfd fds[2];
+	int FDS = 2;
+	struct pollfd *fds;
 
 	if (argc < 3) {
-		printf("usage: %s [tcp|unix] [port|path]\n", argv[0]);
-		return -1;
+		usage(argv);
 	}
 
-	sock = strcasecmp(argv[1], "tcp")? init_unix_listen(argv[2])
-					 : init_tcp_listen(atoi(argv[2]));
+	fds = malloc(FDS * sizeof(struct pollfd));
+	sock = strcasecmp(argv[1], "tcp")?
+	(strcasecmp(argv[1], "unix")? usage(argv) : init_unix_listen(argv[2]))
+	 : init_tcp_listen(atoi(argv[2]));
 
-	if ((client = accept(sock, NULL, NULL)) > -1) {
-		fds[0].fd = client;
-		fds[1].fd = 0;
-		fds[0].events = POLLIN | POLLHUP;
-		fds[1].events = POLLIN | POLLHUP;
+	fds[0].fd = sock;
+	fds[1].fd = 0;
+	fds[0].events = POLLIN | POLLHUP;
+	fds[1].events = POLLIN | POLLHUP;
 
-		while (poll(fds, 2, -1)) {
-			if (fds[0].revents & POLLIN) {
-				r = read(client, buf, sizeof(buf));
+	while (poll(fds, FDS, -1)) {
+		if (fds[0].revents & POLLIN) {
+			for (r = 2; r < FDS; r++)
+				if (fds[r].fd == -1)
+					break;
+			if (r == FDS) {
+				FDS++;
+				fds = realloc(fds, FDS * sizeof(struct pollfd));
+			}
+			client = accept(sock, NULL, NULL);
+			if (client > 0) {
+				fds[r].fd = client;
+				fds[r].events = POLLIN | POLLHUP;
+			} else {
+				perror ("accept");
+			}
+		}
+
+		if (fds[1].revents & POLLIN) {
+			if (fgets(buf, sizeof(buf), stdin) == NULL) {
+				close (client);
+				return 0;
+			}
+
+			for (client = 2; client < FDS; client++) {
+				if (fds[client].fd > 0) {
+					r = write(fds[client].fd, buf, strlen(buf));
+					if (r < 0) {
+						perror("write");
+						close(fds[client].fd);
+						fds[client].fd = -1;
+					}
+				}
+			}
+		}
+
+		for (client = 2; client < FDS; client++) {
+			if (fds[client].fd == -1)
+				continue;
+			if (fds[client].revents & POLLIN) {
+				r = read(fds[client].fd, buf, sizeof(buf));
 				// ctrl+d pressed
 				if (buf[0] == '\x04') {
-					close (client);
-					return 0;
+					close (fds[client].fd);
+					fds[client].fd = -1;
+					continue;
 				}
 
 				if (r < 0) {
 					perror("read");
+					close (fds[client].fd);
+					fds[client].fd = -1;
 					continue;
 				}
 				write(1, buf, r);
 				fflush(stdout);
 			}
-
-			if (fds[1].revents & POLLIN) {
-				if (fgets(buf, sizeof(buf), stdin) == NULL) {
-					close (client);
-					return 0;
-				}
-
-				r = write(client, buf, strlen(buf));
-				if (r < 0)
-					perror("write");
-			}
-
-			for (r = 0; r < 2; r++)
-				if (fds[r].revents & POLLHUP) {
-					close (client);
-					return 0;
-				}
 		}
 
-		perror("poll");
-		close(client);
-		return -1;
+		for (r = 0; r < FDS; r++)
+			if (fds[r].fd != -1 && fds[r].revents & POLLHUP) {
+				close (fds[r].fd);
+				return 0;
+			}
 	}
 
-	perror ("accept");
+	perror("poll");
+	close(client);
 	return -1;
 }
+
