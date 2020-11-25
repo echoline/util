ca73e746e0de3e08add9268b80a3dca5a5307e3e
glenda <glenda@9front.local>
2020-11-25 20:42:28 +0000

    ...

--- volume.c
+++ volume.c
@@ -7,26 +7,21 @@
 #define RATE 44100
 
 int rate;
-double decay;
-int delay;
+double vol;
 Point c;
 
 void
-reverb(unsigned char *buf, short *tmp, int len)
+volume(unsigned char *buf, int len)
 {
 	short l, r;
 	int i;
-	int dur = delay * (rate/1000.0);
 
 	for(i = 0; i < len; i += 4){
 		l = buf[i+0] | (buf[i+1] << 8);
 		r = buf[i+2] | (buf[i+3] << 8);
 
-		l += tmp[(i/2+0+dur)%len] * decay;
-		r += tmp[(i/2+1+dur)%len] * decay;
-
-		tmp[(i/2+0)%len] = l;
-		tmp[(i/2+1)%len] = r;
+		l *= vol;
+		r *= vol;
 
 		buf[i+0] = l & 0xFF;
 		buf[i+1] = (l >> 8) & 0xFF;
@@ -39,13 +34,10 @@ void
 doproc(void *unused)
 {
 	unsigned char *buf = malloc(rate * 4);
-	short *tmp = malloc(rate * 2);
 	int r;
 
-	memset(tmp, 0, rate * 2);
-
 	while((r = read(0, buf, rate * 4)) > 0){
-		reverb(buf, tmp, r);
+		volume(buf, r);
 		write(1, buf, r);
 	}
 }
@@ -53,17 +45,11 @@ doproc(void *unused)
 void
 redraw(Image *screen)
 {
-	int x = delay * 4;
+	int x = c.x * vol;
 
 	draw(screen, screen->r, display->white, nil, ZP);
 
 	draw(screen, Rect(screen->r.min.x + x - 1, screen->r.min.y,
-					screen->r.min.x + x + 1, screen->r.max.y - Dy(screen->r)/2),
-				display->black, nil, ZP);
-
-	x = decay * Dx(screen->r);
-
-	draw(screen, Rect(screen->r.min.x + x - 1, screen->r.min.y + Dy(screen->r)/2,
 					screen->r.min.x + x + 1, screen->r.max.y),
 				display->black, nil, ZP);
 }
@@ -83,13 +69,12 @@ threadmain(int argc, char **argv)
 	Mouse m;
 	Point xy;
 	rate = RATE;
-	delay = 0;
-	decay = 0.0;
+	vol = 1.0;
 
 	if (argc > 1)
 		rate = atoi(argv[1]);
 
-	if(initdraw(0, 0, "reverb") < 0)
+	if(initdraw(0, 0, "volume") < 0)
 		sysfatal("initdraw: %r");
 	einit(Emouse);
 
@@ -100,10 +85,7 @@ threadmain(int argc, char **argv)
 		if(m.buttons & 1){
 			xy = subpt(m.xy, screen->r.min);
 
-			if (xy.y > c.y)
-				decay = xy.x / (double)Dx(screen->r);
-			else
-				delay = xy.x / 4;
+			vol = xy.x / (double)c.x;
 			
 			redraw(screen);
 		}
